4f22e763699283646683ac02be7121f0367b159e
ourooba <bcs163109@cust.pk>
2020-06-20 12:03:42 +0500

    Walletapp

---
+++ src/main/java/com/example/walletapp/service/TransactionService.java
@@ -0,0 +1,57 @@
+package com.example.walletapp.service;
+
+
+import com.example.walletapp.entity.Transaction;
+import com.example.walletapp.entity.Wallet;
+import com.example.walletapp.exception.WalletException;
+import com.example.walletapp.repository.TransactionRepository;
+import com.example.walletapp.repository.WalletRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+@Service
+public class TransactionService {
+    @Autowired
+    private TransactionRepository transactionRepository;
+    @Autowired
+    private WalletRepository walletRepository;
+    public List<Transaction> getAll(Long walletId){
+        Optional<Wallet> wallet = walletRepository.findById(walletId);
+        if(wallet.isPresent()){
+            return transactionRepository.findByWallet(wallet.get());
+        }
+        return null;
+    }
+    public Transaction getById(Long wallet_id,Long id){
+        Optional<Wallet> wallet = walletRepository.findById(wallet_id);
+        if(wallet.isPresent()) {
+            Optional<Transaction> transaction = transactionRepository.findById(id);
+            if (transaction.isPresent()) {
+                return transaction.get();
+            }
+        }
+        throw new WalletException("Transaction with "+id+" does not exists!");
+    }
+    public Transaction createOrUpdate(Long walletId, Transaction transaction){
+        Optional<Wallet> wallet = walletRepository.findById(walletId);
+        if(wallet.isPresent()){
+            transaction.setWallet(wallet.get());
+            transactionRepository.save(transaction);
+            return transaction;
+        }
+        return null;
+    }
+    public boolean delete(Long wallet_id,Long id){
+        Optional<Wallet> wallet = walletRepository.findById(wallet_id);
+        if(wallet.isPresent()) {
+            Optional<Transaction> transaction = transactionRepository.findById(id);
+            if (transaction.isPresent()) {
+                transactionRepository.delete(transaction.get());
+                return true;
+            }
+        }
+        throw new WalletException("Transaction with "+id+" does not exists!");
+    }
+}
