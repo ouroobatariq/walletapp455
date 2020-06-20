190d6fba0574c8b67522d17aadf84f74d644e86d
ourooba <bcs163109@cust.pk>
2020-06-20 13:05:50 +0500

    Walletapp

---
+++ src/main/resources/static/precache-manifest.dcb8101ec90ad008d1bd0c1058de2aa6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e010a99f2b4fcbe4dd483a559a97f0ab",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f3f9692548340ece0d2e",
+    "url": "/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "7df53967ead7a40bbdac",
+    "url": "/static/css/main.00d4352e.chunk.css"
+  },
+  {
+    "revision": "f3f9692548340ece0d2e",
+    "url": "/static/js/2.80f177fa.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.80f177fa.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7df53967ead7a40bbdac",
+    "url": "/static/js/main.154f5d46.chunk.js"
+  },
+  {
+    "revision": "fe35f00a91369b1ee7d4",
+    "url": "/static/js/runtime-main.3f900b9c.js"
+  }
+]);
